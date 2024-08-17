e4b496910c5d5b8bb48c11b963606ef927d136be
ayushmishra9191 <mishraayush9191@gmail.com>
2024-08-17 12:45:47 +0530

    added grade calculator

---
+++ GradeCalculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of subjects: ");
+    int numSubjects = sc.nextInt();
+    if(numSubjects <= 0){
+        System.out.println("please enter a valid number of the subjects.");
+        return;
+}
+int totalMarks = 0;
+int maxMarksPerSubject = 100;
+  
+for(int i = 1; i <= numSubjects; i++){
+    System.out.println("Enter marks obtained in subjects " +i + " (out of 100):");
+    int marks = sc.nextInt();
+
+    if(marks < 0 || marks > maxMarksPerSubject){
+        System.out.println("Marks should be in the range of 0 to 100. Please enter valid marks.");
+        i--;
+    }
+else{
+    totalMarks += marks;
+}
+}
+ 
+double averagePercentage = (double) totalMarks / (numSubjects * maxMarksPerSubject)*100;
+System.out.println("Total Marks: " + totalMarks);
+System.out.println("Average Percentage: " + averagePercentage + "%");
+
+String grade;
+
+if(averagePercentage >= 89){
+    grade = "A+";
+}
+else if (averagePercentage >= 79){
+    grade = "A";
+}
+else if(averagePercentage >=69){
+    grade = "B";
+}
+else if (averagePercentage >=59){
+    grade = "C";
+}
+else if (averagePercentage >=49){
+    grade = "D";
+}
+
+else{
+    grade = "F";
+}
+System.out.println("Grade: " +grade);
+}
+}
